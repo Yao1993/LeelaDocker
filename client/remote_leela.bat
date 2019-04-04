2828168154b097122f9e78936b81cd18082a5633
Yao <tm42h7@hotmail.com>
2019-04-04 23:43:46 +0800

    Add "-r 1" to leelaz

--- client/remote_leela.bat
+++ client/remote_leela.bat
@@ -1,4 +1,4 @@
 call settings.bat
 set BATCH_FILE_DIR=%~dp0
 %BATCH_FILE_DIR%OpenSSH-Win32\ssh.exe root@%HOST_IP% -i %BATCH_FILE_DIR%\.ssh\id_rsa -p %PORT% ^
-    -o UserKnownHostsFile=%BATCH_FILE_DIR%\.ssh\tmp_known_hosts "/leela_zero/src/build/leelaz  --weights /leela_zero/data/best-network.gz --gtp -t %NUM_THREADS%"
+    -o UserKnownHostsFile=%BATCH_FILE_DIR%\.ssh\tmp_known_hosts "/leela_zero/src/build/leelaz  --weights /leela_zero/data/best-network.gz --gtp -t %NUM_THREADS% -r 1"
