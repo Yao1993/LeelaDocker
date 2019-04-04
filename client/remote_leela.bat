c59a652d2776163a3e88e9e7151a46b750e72fb1
Yao <tm42h7@hotmail.com>
2019-04-04 23:42:46 +0800

    Fix path error.

--- client/remote_leela.bat
+++ client/remote_leela.bat
@@ -1,2 +1,4 @@
 call settings.bat
-.\OpenSSH-Win32\ssh.exe root@%HOST_IP% -i .\.ssh\id_rsa -p %PORT% -o UserKnownHostsFile=.\.ssh\tmp_known_hosts /leela_zero/src/build/leelaz  --weights /leela_zero/data/best-network.gz --gtp -t %NUM_THREADS%
+set BATCH_FILE_DIR=%~dp0
+%BATCH_FILE_DIR%OpenSSH-Win32\ssh.exe root@%HOST_IP% -i %BATCH_FILE_DIR%\.ssh\id_rsa -p %PORT% ^
+    -o UserKnownHostsFile=%BATCH_FILE_DIR%\.ssh\tmp_known_hosts "/leela_zero/src/build/leelaz  --weights /leela_zero/data/best-network.gz --gtp -t %NUM_THREADS%"
